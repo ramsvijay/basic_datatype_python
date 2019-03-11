2aa0db44a839ccc8d12fdd392e1a054dae4c1d30
ramsvijay <ramsvijay7@gmail.com>
2019-03-11 12:30:04 +0530

    Modules and package

---
+++ own_module/fileone.py
@@ -0,0 +1,9 @@
+def func():
+    print("Print somethng")
+
+print("Top level print inside fileone.py")
+
+if __name__ =="__main__" :
+     print("if con") 
+else:
+    print("else con")
---
+++ own_module/filetwo.py
@@ -0,0 +1,10 @@
+import fileone
+print("ram")
+
+
+fileone.func()
+
+if __name__ =="__main__":
+    print("if condtion")
+else:
+    print("else condition")    
